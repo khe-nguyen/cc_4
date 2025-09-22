c5651bfcf694d5e0e4ac97b6a7e05689b06c267d
Khe Nguyen <khenguyen@usf.edu>
2025-09-21 22:04:04 -0400

    Created an array with 5 products.

---
+++ cc_4.js
@@ -0,0 +1,13 @@
+// Coding Challenge 4 - Retail Discount Engine: Dynamic Pricing Rules and Inventory Cycling
+
+// Step 2: Product inventory
+let inventory = [
+    { name: "Canon EOS Rebel T7", category: "electronics", price: 450, inventory: 5 },
+    { name: "Jeans", category: "apparel", price: 40, inventory: 10 },
+    { name: "Mochi Ice Cream", category: "groceries", price: 8, inventory: 20 },
+    { name: "Stickers Pack", category: "household", price: 5, inventory: 50 },
+    { name: "Kodak PixPro Camera", category: "electronics", price: 150, inventory: 7 }
+];
+
+
+
