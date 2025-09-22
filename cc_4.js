747926767d47bef2d2a84b0f536af309d5c0ded5
Khe Nguyen <khenguyen@usf.edu>
2025-09-21 22:27:11 -0400

    Applying discounts on each catergory. Tested on console.

--- cc_4.js
+++ cc_4.js
@@ -9,5 +9,23 @@ let inventory = [
     { name: "Kodak PixPro Camera", category: "electronics", price: 150, inventory: 7 }
 ];
 
+// Step 3: Apply discounts on each category
+for (let product of inventory) {
+    switch (product.category) {
+        case "electronics":
+            product.discountedPrice = product.price * 0.8; // 20% off for electronics
+            break;
+        case "apparel":
+            product.discountedPrice = product.price * 0.85; // 15% off for appareal
+            break;
+        case "groceries":
+        case "household":
+            product.discountedPrice = product.price * 0.9; // 10% off for groceries and household
+            break;
+        default:
+            product.discountedPrice = product.price; // no discount
+    }
+}
 
+console.log(inventory[0]);
 
